b98435688466b1423811ae4818ba754181c9ba07
Theo Markettos <git+atm26@cl.cam.ac.uk>
2023-07-31 21:55:50 +0000

    Add the public key to authorized_keys

--- morello/morello_chroot_after.sh
+++ morello/morello_chroot_after.sh
@@ -5,7 +5,7 @@ echo "Morello image customisations..."
 echo "Adding default SSH key"
 sudo mkdir -p "${tempdir}/home/morello/.ssh"
 sudo chmod 700 "${tempdir}/home/morello/.ssh"
-sudo cp -v "${DIR}/morello/morello-bbb" "${tempdir}/home/morello/.ssh/authorized_keys"
+sudo cp -v "${DIR}/morello/morello-bbb.pub" "${tempdir}/home/morello/.ssh/authorized_keys"
 sudo chmod 600 "${tempdir}/home/morello/.ssh/authorized_keys"
 sudo chown -R 1000:1000 "${tempdir}/home/morello/.ssh"
 
